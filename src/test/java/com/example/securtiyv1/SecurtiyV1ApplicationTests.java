501892c9784054c2f1ef16d484b95ff505a94d44
ohyuchan123 <oyuchan50@gmail.com>
2023-03-02 10:41:33 +0900

    Initial commit

---
+++ src/test/java/com/example/securtiyv1/SecurtiyV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securtiyv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurtiyV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
